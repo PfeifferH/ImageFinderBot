5e7946e534de048e24ceae3bd3d0adf61feae1f8
PfeifferH <15hp19@queensu.ca>
2018-06-16 15:20:17 -0400

    Integrated settings.json

--- app/app.js
+++ app/app.js
@@ -1,4 +1,4 @@
-// Load up the discord.js library
+/ Load up the discord.js library
 const Discord = require("discord.js");
 
 // This is your client. Some people call it `bot`, some people call it `self`, 
@@ -6,17 +6,17 @@ const Discord = require("discord.js");
 // this is what we're refering to. Your client.
 const client = new Discord.Client();
 
-// Here we load the config.json file that contains our token and our prefix values. 
-const config = require("./config.json");
-// config.token contains the bot's token
-// config.prefix contains the message prefix.
+// Here we load the settings.json file that contains our token and our prefix values. 
+const settings = require("../settings.json");
+// settings.token contains the bot's token
+// settings.prefix contains the message prefix.
 
 client.on("ready", () => {
   // This event will run if the bot starts, and logs in, successfully.
   console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`); 
   // Example of changing the bot's playing game to something useful. `client.user` is what the
   // docs refer to as the "ClientUser".
-  client.user.setActivity(`Serving ${client.guilds.size} servers`);
+  client.user.setActivity(`Finding the right meme for this`);
 });
 
 client.on("guildCreate", guild => {
@@ -40,14 +40,14 @@ client.on("message", async message => {
   if(message.author.bot) return;
   
   // Also good practice to ignore any message that does not start with our prefix, 
-  // which is set in the configuration file.
-  if(message.content.indexOf(config.prefix) !== 0) return;
+  // which is set in the setting file.
+  if(message.content.indexOf(settings.prefix) !== 0) return;
   
   // Here we separate our "command" name, and our "arguments" for the command. 
   // e.g. if we have the message "+say Is this the real life?" , we'll get the following:
   // command = say
   // args = ["Is", "this", "the", "real", "life?"]
-  const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+  const args = message.content.slice(settings.prefix.length).trim().split(/ +/g);
   const command = args.shift().toLowerCase();
   
   // Let's go with a few common example commands! Feel free to delete or change those.
@@ -134,4 +134,4 @@ client.on("message", async message => {
   }
 });
 
-client.login(config.token);
+client.login(settings.token);
